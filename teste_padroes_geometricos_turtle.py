744328c46ca3856d838f3bff42e1dd61db66eeab
pedrocalderon52 <pedrocalderonn52@gmail.com>
2024-08-10 21:18:51 -0300

    Update teste_padroes_geometricos_turtle.py
    comentei o codigo

--- teste_padroes_geometricos_turtle.py
+++ teste_padroes_geometricos_turtle.py
@@ -6,14 +6,16 @@ colors = {"purple", "black"}
 tl.width(2)
 tl.speed(15)
 """
-#funcao seno
+#sin function
 
 for x in range(100):
     y = mt.sin(x)
     tl.goto(x*5, y*5)
 """
 
-"""for i in range(360):
+"""
+#circle pattern
+for i in range(360):
     tl.right(1)
     tl.up()
     tl.fd(99)
@@ -21,6 +23,7 @@ for x in range(100):
     tl.fd(1)
     tl.teleport(0,0)"""
 
+#ellipse pattern
 for x in range(-20, 21):
     y = abs(30 * (mt.sqrt(1 - ((x**2) / (20**2)))))
     tl.up()
